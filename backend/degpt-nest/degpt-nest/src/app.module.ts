b4c5b6352a404124f1bd42ff3d198d49278c6ca8
maximilianfrederick6 <maximilianfrederick6@gmail.com>
2026-01-12 14:08:34 +0800

    test:test app

--- backend/degpt-nest/degpt-nest/src/app.module.ts
+++ backend/degpt-nest/degpt-nest/src/app.module.ts
@@ -11,7 +11,10 @@ import { OssModule } from './oss/oss.module';
     ConfigModule.forRoot({
       isGlobal: true, // 全局可用，不用每个模块重复导入
       cache: true, // 可选：缓存读取结果
-      // envFilePath: '.env', // 可选：默认就是根目录 .env
+      // 核心修改：不要硬编码 envFilePath
+      // 1. 如果是本地开发，它会自动读取根目录的 .env
+      // 2. 如果是生产环境(Docker)，它会忽略文件（因为没文件），直接读取系统注入的变量
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
     }),
     LargeLanguageModelModule,
     // 改这里：优先读环境变量，兼容 docker 场景
